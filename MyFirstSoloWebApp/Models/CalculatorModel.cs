eb8b1b298be9f2f64d8b8f7b3809940652de2d33
holmes989 <90338056+holmes989@users.noreply.github.com>
2022-01-18 19:41:19 -0700

    Commit

---
+++ MyFirstSoloWebApp/Models/CalculatorModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstSoloWebApp.Models
+{
+    public class CalculatorModel
+    {
+        [Range(0, 100)]
+        public int Assign { get; set; }
+
+        [Range(0, 100)]
+        public int Grp { get; set; }
+
+        [Range(0, 100)]
+        public int Qz { get; set; }
+
+        [Range(0, 100)]
+        public int Xm { get; set; }
+
+        [Range(0, 100)]
+        public int Ntx { get; set; }
+    }
+}
--- MyFirstSoloWebApp/wwwroot/js/GradeCalc.js
+++ MyFirstSoloWebApp/wwwroot/js/GradeCalc.js
@@ -1,33 +1,34 @@
 ﻿/*Makes a button*/
 $("#btnSend").click(function () {
-//Creates the value of total grade based on inputs
-totalGrade = (($("#txtAssign").val() * .55) + ($("#txtGrp").val() * .05) + ($("#txtQz").val() * .1) + ($("#txtXm").val() * .2) + ($("#txtNtx").val() * .1))
-//Creates total letter grade and assigns it the be the letter grade according to the total grade
+    //Creates the value of total grade based on inputs
+    totalGrade = (($("#txtAssign").val() * .55) + ($("#txtGrp").val() * .05) + ($("#txtQz").val() * .1) + ($("#txtXm").val() * .2) + ($("#txtNtx").val() * .1))
+    //Creates total letter grade and assigns it the be the letter grade according to the total grade
     letterGrade = ""
-if ((totalGrade) >= 94) {
-    letterGrade = "A";
-} else if ((totalGrade) >= 90) {
-    letterGrade = "A-";
-} else if ((totalGrade) >= 87) {
-    letterGrade = "B+";
-} else if ((totalGrade) >= 84) {
-    letterGrade = "B";
-} else if ((totalGrade) >= 80) {
-    letterGrade = "B-";
-} else if ((totalGrade) >= 77) {
-    letterGrade = "C+";
-} else if ((totalGrade) >= 74) {
-    letterGrade = "C";
-} else if ((totalGrade) >= 70) {
-    letterGrade = "C-";
-} else {
-    letterGrade = "F";
-}
+    if ((totalGrade) >= 94) {
+        letterGrade = "A";
+    } else if ((totalGrade) >= 90) {
+        letterGrade = "A-";
+    } else if ((totalGrade) >= 87) {
+        letterGrade = "B+";
+    } else if ((totalGrade) >= 84) {
+        letterGrade = "B";
+    } else if ((totalGrade) >= 80) {
+        letterGrade = "B-";
+    } else if ((totalGrade) >= 77) {
+        letterGrade = "C+";
+    } else if ((totalGrade) >= 74) {
+        letterGrade = "C";
+    } else if ((totalGrade) >= 70) {
+        letterGrade = "C-";
+    } else {
+        letterGrade = "F";
+    }
+
+    alert("Total Grade: " + totalGrade + " Letter Grade: " + letterGrade);
     //Creates the button that will display the total grade and the letter grade
-    alert("Total Grade: " + totalGrade + " Letter Grade: " + letterGrade)
-        //$("#txtAssign").val() + 
-        //$("#txtGrp").val() +
-        //$("#txtQz").val() +
-        //$("#txtXm").val() +
-        //$("#txtNtx").val())
 })
+//Assignments 55 %
+//Group Project 5 %
+//Quizzes 10 %
+//Exams 20 %
+//INTEX 10 %
